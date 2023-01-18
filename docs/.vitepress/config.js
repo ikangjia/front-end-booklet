dad781345812b6ad2458c233ec3e8f27345c1c14
kangjia <ikangjia.cn@outlook.com>
2023-01-18 22:48:54 +0800

    重构目录

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -15,20 +15,20 @@ export default {
                 {
                     text: '前端工程化入门教程',
                     items: [
-                        {text: '了解前端工程化', link: '/engineering/了解前端工程化.md'},
-                        {text: '学会使用包管理工具', link: '/engineering/学会使用包管理工具.md'},
-                        {text: '脚手架与工具链', link: '/engineering/脚手架与工具链.md'},
-                        {text: '论一个组件的自我修养', link: '/engineering/组件的自我修养.md'},
-                        {text: '组件之间的通信', link: '/engineering/组件间的通信.md'},
-                        {text: '全局状态的管理', link: '/engineering/全局状态的管理.md'},
-                        {text: '聊聊代码规范', link: '/engineering/学会使用包管理工具.md'},
-                        {text: '高效开发', link: '/engineering/高效开发.md'},
+                        {text: '了解前端工程化', link: '/vue/了解前端工程化.md'},
+                        {text: '学会使用包管理工具', link: '/vue/学会使用包管理工具.md'},
+                        {text: '脚手架与工具链', link: '/vue/脚手架与工具链.md'},
+                        {text: '论一个组件的自我修养', link: '/vue/组件的自我修养.md'},
+                        {text: '组件之间的通信', link: '/vue/组件间的通信.md'},
+                        {text: '全局状态的管理', link: '/vue/全局状态的管理.md'},
+                        {text: '聊聊代码规范', link: '/vue/学会使用包管理工具.md'},
+                        {text: '高效开发', link: '/vue/高效开发.md'},
                     ]
                 },
                 {
                     text: '搞不懂的 JS',
                     items: [
-                        {text: '快速上手 TypeScript', link: '/js/快速上手 TypeScript.md'},
+                        {text: '快速上手 TypeScript', link: '/js/typeScript.md'},
                         {text: '实用代码技巧', link: '/js/实用代码技巧.md'},
                         {text: '数组', link: '/js/数组.md'},
                         {text: '字符串', link: '/js/字符串.md'},
@@ -47,10 +47,10 @@ export default {
                 }
             ],
         },
-        // editLink: {
-        //     pattern: 'https://github.com/ikangjia/vitepress/edit/main/docs/:path',
-        //     text: '在 GitHub 上编辑本章内容'
-        // },
+        editLink: {
+            pattern: 'https://github.com/ikangjia/front-end-booklet/edit/main/docs/:path',
+            text: '在 GitHub 上编辑本章内容'
+        },
         docFooter: {
             prev: '上一章',
             next: '下一章'
